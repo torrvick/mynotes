08da2ea6ad00c09b78c4b425ec422346c13d0afc
root <root@plane.local>
2023-12-11 08:36:24 +0000

    Added Readme.md

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,20 +1,112 @@
-export function addNote(title, content) {
-    const title = titleInput.value;
-    const content = contentInput.value;
-
-    fetch('/api/notes', {
+// export function addNote(content) {
+//     fetch('/api/notes', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ content })
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//         })
+//         .catch(error => console.error(error));
+// }
+export function addNote(content) {
+    return fetch('/api/notes', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ title, content })
+        body: JSON.stringify({ content })
     })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            titleInput.value = '';
-            contentInput.value = '';
-            getNotes();
+            // console.log(data);
+            return data;
         })
-        .catch(error => console.error(error));
+        .catch(error => {
+            console.error(error);
+            throw error; // Пробросить ошибку для обработки в вызывающем коде
+        });
+}
+
+export const getNotes = async () => {
+    try {
+        const response = await fetch('/api/notes');
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const getOneNote = async (noteId) => {
+    try {
+        const response = await fetch('/api/notes/' + noteId);
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//     try {
+//         const response = await fetch('/api/notes/' + id, {
+//             method: 'DELETE'
+//         });
+//         const data = await response.json();
+//         console.log(data);
+//         return data;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }; 
+
+// export function deleteNote(id) {
+//     try {
+//         response = await fetch('/api/notes/' + id, {
+//             method: 'DELETE'
+//         });
+//         const data = await response.json();
+//         console.log(data);
+//         return data;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
+
+export function deleteNote(id) {
+    return fetch('/api/notes/' + id, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            return data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+export function editNote(id, content) {
+    console.log(JSON.stringify({ content }));
+    return fetch('/api/notes/' + id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ content })
+    })
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            return data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
